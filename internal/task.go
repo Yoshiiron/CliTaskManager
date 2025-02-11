02dbdc8969f49d8183a402afe7236da4706e100a
yoshiiiron <eynrus@gmail.com>
2025-02-11 08:04:57 +0300

    change utils folder to internal

---
+++ internal/task.go
@@ -0,0 +1,9 @@
+package internal
+
+type Task struct {
+	Id          int    `json:"id"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
+}
